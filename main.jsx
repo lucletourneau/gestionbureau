9633134593d002447c746b41b336250b5f0b40cd
lucletourneau <luc.letourneau@voyagedor.com>
2026-01-05 17:01:23 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app' // On utilise './app' sans l'extension .jsx pour plus de compatibilité
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app'; // Cherche exactement le fichier app.jsx à la racine
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// On récupère l'élément <div id="root"></div> de ton index.html
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("L'élément 'root' est introuvable dans le fichier index.html");
+}
