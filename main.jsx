6e54d816ce484314cf8194f10ad9ace5fe1296c9
lucletourneau <luc.letourneau@voyagedor.com>
2026-01-05 16:50:46 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.'
+import App from './app' // On utilise './app' sans l'extension .jsx pour plus de compatibilité
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
